1b9ed9d47e9cafcac5b7f714f696e08fda1c7566
Bryan Lucas Cabral Do Nascimento <bryan.brasil.bl@gmail.com>
2022-07-05 14:32:25 -0300

    metodo concat

---
+++ arrays/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['fulana', 'ciclana']
+const filhos = ['fulano', 'ciclano']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2],[3,4],5,[[6,4]]))
